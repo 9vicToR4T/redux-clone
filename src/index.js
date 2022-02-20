7fc27b2ab4b8b93023a2b3fd89bbdc18855535f8
victor <vt@mail.com>
2022-02-20 15:29:33 +0000

    A simple program for understanding better redux and how it work. Interesting new things about function oriented programming. Observer pattern. Currying. Composition.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,61 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useEffect, useState } from "react";
+import ReactDOM from "react-dom";
+import { createStore } from "./store/createStore";
+import { taskReducer } from "./store/taskReducer";
+import * as actions from './store/actionTypes';
+
+const initialStore =  [
+  { id: 1, title: "Task 1", completed: false },
+  { id: 2, title: "Task 2", completed: false },
+  { id: 3, title: "Task 3", completed: false },
+]
+
+const store = createStore(taskReducer, initialStore);
+
+const App = () => {
+  const [state, setState] = useState(store.getState());
+
+  const handleComplete = (id) => {
+    store.dispatch({
+      type: actions.taskUpdated,
+      payload: { id: id, completed: true},
+    });
+  };
+
+  const changeTitle = (taskId) => {
+    store.dispatch({
+      type: actions.taskUpdated,
+      payload: { id: taskId, title: `New title ${taskId}` },
+    });
+  };
+
+  useEffect(() => {
+    store.subscribe(() => {
+      setState(store.getState());
+    });
+  }, []);
+
+  return (
+    <>
+      <h1>Redux Clone </h1>
+      <ul>
+        {state.map((el) => (
+          <li key={el.id}>
+            <p> {el.title} </p>
+            <p>{`Completed: ${el.completed}`}</p>
+            <button onClick={() => handleComplete(el.id)}>Complete</button>
+            <button onClick={() => changeTitle(el.id)}>Change title</button>
+            <hr />
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+};
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
---
+++ src/store/createStore.js
@@ -0,0 +1,24 @@
+export function createStore(reducer, initialState) {
+  let state = initialState;
+  let listeners = [];
+
+  function getState() {
+    return state;
+  }
+
+  function dispatch(action) {
+    state = reducer(state, action);
+    // atunci cand schimbam state, avem nevoie sa chemam listeners pentru ca ei in continuare sa schimbe interface. Observer pattern
+    // fiecare listener e o functie in cazul dat
+    for (let i = 0; i < listeners.length; i++) {
+      const listener = listeners[i];
+      listener();
+    }
+  }
+
+  function subscribe(listener) {
+    listeners.push(listener);
+  }
+
+  return { getState, dispatch, subscribe };
+}
